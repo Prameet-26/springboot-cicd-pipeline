6b1eb02eabc5a48afb775de42a6b8dc4dde5d834
Prameet Kumar <imprameet@gmail.com>
2026-07-06 06:52:55 +0000

    Added new feature

---
+++ springboot-cicd-pipeline/src/test/java/com/prameet/springboot_cicd_pipeline/SpringbootCicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prameet.springboot_cicd_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
